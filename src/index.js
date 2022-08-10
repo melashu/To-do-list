e2ceea7f4650cd394e0d9a9fa15bc63ebf868104
melashu <meshu102@outlook.com>
2022-08-10 16:29:21 +0300

    Add class for new list and add checked property to checkbox

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.css';
+import "./style.css";
 
 class Task {
   constructor(description, completed, index) {
@@ -9,25 +9,39 @@ class Task {
 }
 
 const toDoList = [
-  new Task('Wash the disc', false, 0),
-  new Task('Read about JS', false, 1),
-  new Task('Walk with my girlfrind', false, 2),
-  new Task('do my to do list app', false, 3),
-  new Task('Read Book', false, 4),
+  new Task("Wash the disc", false, 0),
+  new Task("Read about JS", false, 1),
+  new Task("Walk with my girlfrind", false, 2),
+  new Task("do my to do list app", false, 3),
+  new Task("Read Book", false, 4),
 ];
 
 function displayTodoList() {
-  const toDo = document.querySelector('.to-do-list');
+  const toDo = document.querySelector(".to-do-list");
   toDo.innerHTML = `<li><p class="todo-title">Todays To Do</p></li>
             <li><input type="text" id="todo-box" placeholder="Add to your list..."></li>`;
-  toDoList.forEach((task) => {
-    const li = document.createElement('li');
+  toDoList.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.className = "list";
     li.innerHTML = `
-            <input type="checkbox"  id="complete" class="complete">
-            ${task.description} <i class="fa-thin fa-ellipsis-vertical"></i>
+            <input type="checkbox" class="complete" id="${task.index}">
+           <div class="before"> <h4>${task.description} </h4><i class="fa-solid fa-ellipsis-vertical"></i></div>
          `;
+
     toDo.appendChild(li);
+    // const chInput = document.getElementById(task.index);
+    // console.log(task.index);
+    // chInput.setAttribute("checked", task.completed);
   });
 }
 
-document.addEventListener('DOMContentLoaded', displayTodoList);
+displayTodoList();
+const container = document.querySelectorAll(".list");
+
+for (let i = 0; i < toDoList.length; i++) {
+  container[i].firstElementChild.checked = toDoList[i].completed;
+}
+
+// console.log(container.firstElementChild)
+
+// document.addEventListener('DOMContentLoaded', displayTodoList);
