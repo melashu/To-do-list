f089162f4998923170920bc1416871c15a3353d6
melashu <meshu102@outlook.com>
2022-08-12 09:33:39 +0300

    Add event listners

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
-import './style.css';
-import TaskStorage from '../js/TaskStorage;'
-import UI from '../js/UI';
-
+import "./style.css";
+import TaskStorage from "../js/TaskStorage.js";
+import UI from "../js/UI.js";
+import Task from "../js/Task";
 
 UI.displayTodoList();
 
-const container = document.querySelectorAll('.list');
+const container = document.querySelectorAll(".list");
 let toDoList = TaskStorage.getAllTask();
 for (let i = 0; i < toDoList.length; i += 1) {
   container[i].firstElementChild.checked = toDoList[i].completed;
 }
+
+const toDo = document.querySelector(".form");
+toDo.addEventListener("change", () => {
+  let taskInput = document.getElementById("todo-box").value;
+  TaskStorage.addNewTask(taskInput);
+});
+
+const lists = document.querySelectorAll(".list");
+
+lists.forEach((list) => {
+  list.addEventListener("focusin", (e) => {
+    let form = list.children[1];
+    let inputBox = form.children[0];
+    let deleteIcon = form.children[1];
+    let menuIcon = form.children[2];
+
+    deleteIcon.style.display = "block";
+    menuIcon.style.display = "none";
+
+    list.style.backgroundColor = "#f2f2f2";
+    inputBox.style.backgroundColor = "#f2f2f2";
+  });
+});
+
+lists.forEach((list, index) => {
+  list.addEventListener("focusout", (e) => {
+    let form = list.children[1];
+    let inputBox = form.children[0];
+    let deleteIcon = form.children[1];
+    let menuIcon = form.children[2];
+    let hiddenInput = form.children[3];
+
+    deleteIcon.style.display = "none";
+    menuIcon.style.display = "block";
+
+    list.style.backgroundColor = "";
+    inputBox.style.backgroundColor = "";
+
+    if (inputBox.value !== hiddenInput.value) {
+      TaskStorage.updateTask(inputBox.value, parseInt(inputBox.id));
+    }
+  });
+});
+
+document.querySelector(".delete-icon").addEventListener('click', () => {
+  alert('delete')
+});
