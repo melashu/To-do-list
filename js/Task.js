437be5df632edb18d03c0819dbe650fb2f0ee732
melashu <meshu102@outlook.com>
2022-08-12 09:29:53 +0300

    Chanfe the positions of default parameter

--- js/Task.js
+++ js/Task.js
@@ -1,5 +1,5 @@
 export default class Task {
-  constructor(description, completed=false, index) {
+  constructor(description, index, completed = false) {
     this.description = description;
     this.completed = completed;
     this.index = index;
