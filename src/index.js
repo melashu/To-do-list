6210c2fee73edfae90883c9456934222b47c5db8
melashu <meshu102@outlook.com>
2022-08-12 16:24:13 +0300

    Call Managetask clsas methods

--- src/index.js
+++ src/index.js
@@ -1,50 +1,48 @@
-import './style.css';
-import TaskStorage from '../module/TaskStorage.js';
-import UI from '../module/UI.js';
+import "./style.css";
+import TaskStorage from "../module/TaskStorage.js";
+import UI from "../module/UI.js";
+import ManageTask from "../module/ManageTask.js";
 
 UI.displayTodoList();
+ManageTask.fillCheckBox();
+ManageTask.addEventToCheckBox();
+ManageTask.cleanAll();
 
-const container = document.querySelectorAll('.list');
-const toDoList = TaskStorage.getAllTask();
-for (let i = 0; i < toDoList.length; i += 1) {
-  container[i].firstElementChild.checked = toDoList[i].completed;
-}
-
-const toDo = document.querySelector('.form');
-toDo.addEventListener('change', () => {
-  const taskInput = document.getElementById('todo-box').value;
+const toDo = document.querySelector(".form");
+toDo.addEventListener("change", () => {
+  const taskInput = document.getElementById("todo-box").value;
   TaskStorage.addNewTask(taskInput);
   window.location.reload();
 });
 
-const lists = document.querySelectorAll('.list');
+const lists = document.querySelectorAll(".list");
 
 lists.forEach((list) => {
-  list.addEventListener('click', () => {
+  list.addEventListener("click", () => {
     const form = list.children[1];
     const inputBox = form.children[0];
     const deleteIcon = form.children[1];
     const menuIcon = form.children[2];
-    deleteIcon.style.display = 'block';
-    menuIcon.style.display = 'none';
-    list.style.backgroundColor = '#f2f2f2';
-    inputBox.style.backgroundColor = '#f2f2f2';
+    deleteIcon.style.display = "block";
+    menuIcon.style.display = "none";
+    list.style.backgroundColor = "#f2f2f2";
+    inputBox.style.backgroundColor = "#f2f2f2";
   });
 });
 
 lists.forEach((list) => {
-  list.addEventListener('change', () => {
+  list.addEventListener("change", () => {
     const form = list.children[1];
     const inputBox = form.children[0];
     const deleteIcon = form.children[1];
     const menuIcon = form.children[2];
     const hiddenInput = form.children[3];
 
-    deleteIcon.style.display = 'none';
-    menuIcon.style.display = 'block';
+    deleteIcon.style.display = "none";
+    menuIcon.style.display = "block";
 
-    list.style.backgroundColor = '';
-    inputBox.style.backgroundColor = '';
+    list.style.backgroundColor = "";
+    inputBox.style.backgroundColor = "";
 
     if (inputBox.value !== hiddenInput.value) {
       TaskStorage.updateTask(inputBox.value, Number(inputBox.id));
@@ -53,22 +51,22 @@ lists.forEach((list) => {
 });
 
 lists.forEach((list) => {
-  list.addEventListener('focusout', () => {
+  list.addEventListener("focusout", () => {
     const form = list.children[1];
     const inputBox = form.children[0];
     const deleteIcon = form.children[1];
     const menuIcon = form.children[2];
 
-    deleteIcon.style.display = 'none';
-    menuIcon.style.display = 'block';
+    deleteIcon.style.display = "none";
+    menuIcon.style.display = "block";
 
-    list.style.backgroundColor = '';
-    inputBox.style.backgroundColor = '';
+    list.style.backgroundColor = "";
+    inputBox.style.backgroundColor = "";
   });
 });
 
-document.querySelectorAll('.delete-icon').forEach((del) => {
-  del.addEventListener('click', (e) => {
+document.querySelectorAll(".delete-icon").forEach((del) => {
+  del.addEventListener("click", (e) => {
     const li = e.target.parentElement.parentElement;
     li.remove();
     const index = e.target.id;
@@ -76,6 +74,6 @@ document.querySelectorAll('.delete-icon').forEach((del) => {
   });
 });
 
-document.querySelector('.referesh').addEventListener('click', () => {
+document.querySelector(".referesh").addEventListener("click", () => {
   window.location.reload();
 });
