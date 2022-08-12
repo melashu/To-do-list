1928e54e2346b752a8c0b7965fb7b279c5520ab1
melashu <meshu102@outlook.com>
2022-08-12 14:32:45 +0300

    fix linter error

--- src/index.js
+++ src/index.js
@@ -1,81 +1,76 @@
-import "./style.css";
-import TaskStorage from "../js/TaskStorage.js";
-import UI from "../js/UI.js";
-import Task from "../js/Task";
+import './style.css';
+import TaskStorage from '../js/TaskStorage.js';
+import UI from '../js/UI.js';
 
 UI.displayTodoList();
 
-const container = document.querySelectorAll(".list");
-let toDoList = TaskStorage.getAllTask();
+const container = document.querySelectorAll('.list');
+const toDoList = TaskStorage.getAllTask();
 for (let i = 0; i < toDoList.length; i += 1) {
   container[i].firstElementChild.checked = toDoList[i].completed;
 }
 
-const toDo = document.querySelector(".form");
-toDo.addEventListener("change", () => {
-  let taskInput = document.getElementById("todo-box").value;
+const toDo = document.querySelector('.form');
+toDo.addEventListener('change', () => {
+  const taskInput = document.getElementById('todo-box').value;
   TaskStorage.addNewTask(taskInput);
 });
 
-const lists = document.querySelectorAll(".list");
+const lists = document.querySelectorAll('.list');
 
 lists.forEach((list) => {
-  list.addEventListener("click", (e) => {
-    let form = list.children[1];
-    let inputBox = form.children[0];
-    let deleteIcon = form.children[1];
-    let menuIcon = form.children[2];
-    deleteIcon.style.display = "block";
-    menuIcon.style.display = "none";
-    list.style.backgroundColor = "#f2f2f2";
-    inputBox.style.backgroundColor = "#f2f2f2";
+  list.addEventListener('click', () => {
+    const form = list.children[1];
+    const inputBox = form.children[0];
+    const deleteIcon = form.children[1];
+    const menuIcon = form.children[2];
+    deleteIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
+    list.style.backgroundColor = '#f2f2f2';
+    inputBox.style.backgroundColor = '#f2f2f2';
   });
 });
 
 lists.forEach((list) => {
-  list.addEventListener("change", (e) => {
-    let form = list.children[1];
-    let inputBox = form.children[0];
-    let deleteIcon = form.children[1];
-    let menuIcon = form.children[2];
-    let hiddenInput = form.children[3];
+  list.addEventListener('change', () => {
+    const form = list.children[1];
+    const inputBox = form.children[0];
+    const deleteIcon = form.children[1];
+    const menuIcon = form.children[2];
+    const hiddenInput = form.children[3];
 
-    deleteIcon.style.display = "none";
-    menuIcon.style.display = "block";
+    deleteIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
 
-    list.style.backgroundColor = "";
-    inputBox.style.backgroundColor = "";
+    list.style.backgroundColor = '';
+    inputBox.style.backgroundColor = '';
 
     if (inputBox.value !== hiddenInput.value) {
-      TaskStorage.updateTask(inputBox.value, parseInt(inputBox.id));
+      TaskStorage.updateTask(inputBox.value, Number(inputBox.id));
     }
   });
 });
 
 lists.forEach((list) => {
-  list.addEventListener("focusout", (e) => {
-    let form = list.children[1];
-    let inputBox = form.children[0];
-    let deleteIcon = form.children[1];
-    let menuIcon = form.children[2];
-    let hiddenInput = form.children[3];
+  list.addEventListener('focusout', () => {
+    const form = list.children[1];
+    const inputBox = form.children[0];
+    const deleteIcon = form.children[1];
+    const menuIcon = form.children[2];
 
-    deleteIcon.style.display = "none";
-    menuIcon.style.display = "block";
+    deleteIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
 
-    list.style.backgroundColor = "";
-    inputBox.style.backgroundColor = "";
+    list.style.backgroundColor = '';
+    inputBox.style.backgroundColor = '';
   });
 });
 
-
-
-document.querySelectorAll(".delete-icon").forEach((del) => {
-  del.addEventListener("click", (e) => {
-    let li = e.target.parentElement.parentElement;
+document.querySelectorAll('.delete-icon').forEach((del) => {
+  del.addEventListener('click', (e) => {
+    const li = e.target.parentElement.parentElement;
     li.remove();
-    let index = e.target.id;
+    const index = e.target.id;
     TaskStorage.deleteTask(index);
- 
   });
 });
