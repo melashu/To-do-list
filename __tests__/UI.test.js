62db3237f67fb244ce3c02117ca5c0b0674a757c
melashu <meshu102@outlook.com>
2022-08-18 20:02:27 +0300

    Create test file for add and remove task

---
+++ __tests__/UI.test.js
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+import UI from "../module/UI";
+
+describe("Add Task to the task list", () => {
+  test("Add New Task", () => {
+    // document.body.innerHTML = "<div>" + '  <ul id="list"></li>' + "</div>";
+    // // UI.addTaskToScreen("Read JS Module", 0);
+    // const list = document.querySelectorAll("#list li");
+    expect(1).toBe(1);
+  });
+});
